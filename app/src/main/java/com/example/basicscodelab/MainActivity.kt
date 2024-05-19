5e9d63e36901137efe14fc9aaee0ea7f673cff09
jungspin <jungspinn@gmail.com>
2024-05-19 18:40:06 +0900

    튜토리얼 마무리

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,28 +1,42 @@
 package com.example.basicscodelab
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
@@ -32,22 +46,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun OnboardingScreen(modifier: Modifier = Modifier) {
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }
-
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -55,7 +61,7 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
     ) {
         Text("Welcome to the Basics Codelab!")
         Button(
-            onClick = { shouldShowOnBoarding = false },
+            onClick = onContinueClicked,
             modifier = Modifier.padding(vertical = 24.dp)
         ) {
             Text(text = "Continue")
@@ -65,59 +71,122 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun MyApp(
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) {
+        if (shouldShowOnBoarding) {
+            OnboardingScreen { shouldShowOnBoarding = false }
+        } else {
+            Greetings()
+        }
+
+    }
+}
+
+@Composable
+fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("James", "Jane"),
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background,
-    ) {
-        Column(modifier = modifier.padding(vertical = 4.dp)) {
-            for (name in names) {
-                Greeting(name = name)
-            }
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        // androidx.compose.foundation.lazy.items
+        items(items = names) { name ->
+            Greeting(name = name)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember { mutableStateOf(false) }
-    // extraPadding은 간단한 계산을 실행하므로 리컴포지션에 대비해 이 값을 기억할 필요 X
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp, horizontal = 8.dp)
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = modifier.padding(24.dp)) {
-            Column(
-                modifier = modifier
-                    .weight(0.5f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello ")
-                Text(text = "${name}!")
-            }
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value },
-                modifier = modifier.weight(0.5f)
-            ) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+        CardContent(name = name)
+    }
+}
+
+@Composable
+fun CardContent(name: String, modifier: Modifier = Modifier) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    // extraPadding은 간단한 계산을 실행하므로 리컴포지션에 대비해 이 값을 기억할 필요 X
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
+        label = ""
+    )
+
+
+    Row(
+        modifier = modifier
+            .padding(24.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = modifier
+                .weight(1f)
+        ) {
+            Text(text = "Hello ")
+            Text(
+                text = "${name}!", style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
         }
+        IconButton(onClick = { expanded = !expanded }, modifier = modifier) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) stringResource(R.string.show_less)
+                else stringResource(R.string.show_more),
+            )
+        }
+    }
 
+}
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun GreetingPreview() {
+    BasicsCodelabTheme {
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    BasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
     }
 }
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun GreetingPreview() {
+fun OnboardingPreview() {
     BasicsCodelabTheme {
-        OnboardingScreen()
+        OnboardingScreen {}
     }
 }
